6b79f5b21aa21c20019b2d9d35d8081fc5b132ab
Megan Kaluza <megan.kaluza@gmail.com>
2016-10-14 09:56:00 -0700

    Outputs user name pulled from input of user name

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -6,12 +6,14 @@ $(document).ready(function(){
     var name = $("#name").val();
     $("#name").val("");
     var newTemplate = new Template(name);
-    $("#output-name").text("Welcome " + newTemplate.getUsername() + "!");
+    //$("#output-name").text("Welcome " + response.name + "!");
 
-    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+    $.get('https://api.github.com/users/megankaluza?access_token=' + apiKey).then(function(response){
       console.log(response);
         console.log (response.public_repos);
           console.log(response.email);
+
+            $("#output-name").text("Welcome " + response.name + "!" + response.email);
     }).fail(function(error){
       console.log('.public_repos');
     });
