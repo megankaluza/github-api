ee216711a73ebb38281a6315dc1f0cb3796ce4f9
Megan Kaluza <megan.kaluza@gmail.com>
2016-10-14 09:07:32 -0700

    API key works in console.log

--- js/github.js
+++ js/github.js
@@ -1,10 +1,13 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+// var apiKey = require('./../.env').apiKey;
+//
+//
+// exports.getRepos = function(){
+//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
 
 
 Template = function(name) {
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,9 +1,17 @@
 var Template = require('./../js/github.js').templateModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
   $("#getUsername").click(function(){
     var name = $("#name").val();
     var newTemplate = new Template(name);
     $("#output-name").text(newTemplate.getUsername());
+    $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+      console.log(response);
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+    console.log(name);
+    console.log(error.responseJSON.message);
   });
 });
