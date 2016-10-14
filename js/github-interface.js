f3436e9bdc3b06e68812c7b9ccd68017fc088186
Megan Kaluza <megan.kaluza@gmail.com>
2016-10-14 09:42:35 -0700

    Outputs users responses

--- js/github-interface.js
+++ js/github-interface.js
@@ -4,14 +4,16 @@ var apiKey = require('./../.env').apiKey;
 $(document).ready(function(){
   $("#getUsername").click(function(){
     var name = $("#name").val();
+    $("#name").val("");
     var newTemplate = new Template(name);
-    $("#output-name").text(newTemplate.getUsername());
+    $("#output-name").text("Welcome " + newTemplate.getUsername() + "!");
+
     $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
       console.log(response);
+        console.log (response.public_repos);
+          console.log(response.email);
     }).fail(function(error){
-      console.log(error.responseJSON.message);
+      console.log('.public_repos');
     });
-    console.log(name);
-    console.log(error.responseJSON.message);
   });
 });
