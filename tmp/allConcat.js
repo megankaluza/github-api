066461ecae276cad230d4d8ce8ebda95d96b9d31
megxkae xo <megxkae xo>
2016-10-14 18:08:31 -0700

    Style changes and appending repositories

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -10,19 +10,18 @@ $(document).ready(function(){
     $.get('https://api.github.com/users/' + name + '?access_token=' + apiKey).then(function(response){
       $("#avatar").empty();
       console.log(response);
-        $("#output-name").text(response.name);
-        $("#avatar").append("<img src=" + response.avatar_url + ">");
+      $("#output-name").text(response.name);
+      $("#avatar").append("<img src=" + response.avatar_url + ">");
     }).fail(function(error){
     });
 
-//     $.get('https://api.github.com/users/' + name + '/repos?visibility=public&access_token=' + apiKey).then(function(response){
-//       // $("#repos").empty();
-//       console.log(response);
-//    for (i = 0; i < response.length; i++) {
-//       $("#repos").append("<a target='_blank' href=' + response[i].html_url ' + response[i].name + response[i].description + ""</a>");
-// };
-//     console.log(response);
-//   }).fail(function(error){
-//    });
-});
+    $.get('https://api.github.com/users/' + name + '/repos?visibility=public&access_token=' + apiKey).then(function(response){
+      $("#repos").empty();
+      console.log(response);
+      for (i = 0; i < response.length; i++) {
+        $("#repos").append("<h2><a href='" + response[i].html_url + "' target='_blank'>Repository</a>: <br><span>" + response[i].name + "</span></h2><p>" + response[i].description + "</p>");
+      };
+    }).fail(function(error){
+    });
+  });
 });
